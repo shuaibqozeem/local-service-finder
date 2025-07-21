38b9070eb52291a4f01596812ef41dd5cd32c740
shuaib qozeem <shuaibqozeem123@gmail.com>
2025-07-21 09:37:08 -0700

    initial commit

--- public/login.js
+++ public/login.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Password:", passwordValue); // Log the password value
 
       try {
-        const response = await fetch("http://localhost:5000/login", {
+        const response = await fetch("https://local-service-finder.onrender.com/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
